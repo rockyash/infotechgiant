1c358dfb8cce9829d8cb2be3c4ac728375fb95f3
rockyash <66306817+rockyash@users.noreply.github.com>
2020-08-18 12:11:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,341 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="index.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script type="text/javascript" src="index.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet'>
+    <!-- sticky icons -->
+    <link rel="stylesheet" type="text/css" href="sticky.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <title>InfoTech Giant</title>
+</head>
+
+<body>
+
+
+
+
+
+    <!-- sticky icons -->
+    <div class="icons">
+        <ul>
+
+
+            <a href="https://www.facebook.com/InfoTechGiantOfficial">
+                <li class="facebook"><i class="fa fa-facebook"></i></li>
+            </a>
+            <a href="#">
+                <li class="twitter"><i class="fa fa-twitter"></i></li>
+            </a>
+            <a href="#">
+                <li class="linkedin"><i class="fa fa-linkedin"></i></li>
+            </a>
+            <a href="#">
+                <li class="instagram"><i class="fa fa-instagram"></i></li>
+            </a>
+
+        </ul>
+    </div>
+
+
+
+
+
+
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-warning ">
+        <a class="navbar-brand" href="#"><strong class="company">InfoTech Giant</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li> -->
+                <li class="nav-item">
+                    <a class="nav-link" href="services.php">Services</a>
+                </li>
+               
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+            
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center parallax">
+                <h1 class="text-white">INFOTECH GIANT</h1>
+                <p>
+                <h4 class="normaltext">your business. A website not only represents your business but it helps you to
+                    build trust professionally and expand your business globally. We are team of professionals having
+                    world class designers with latest technologies and frameworks which are affordable for our clients.
+                    Our mission at InfoTech Giant is not only limited to just create
+                    websites but we aim to add value to your business</h4>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="container">
+        <h1 class="text-capitalize text-center" style="font-family: roboto">Contact Us</h1>
+        <hr>
+    <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form> -->
+
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+
+
+
+    <!-- Contact Us Section
+    <div class="container">
+
+        <h3 class="text-center">Contact us</h3><br />
+
+        <div class="row">
+            <div class="col-md-8">
+                <form action="index.php" method="post">
+                    <input class="form-control" type="text" name="name" placeholder="Name..."  required ><br>
+                    <input class="form-control" type="varchar" name="phone" placeholder="Phone..." ><br />
+                    <input class="form-control" typr="email" name="email" placeholder="E-mail..." ><br />
+                    <textarea class="form-control" type="text" name="message" placeholder="How can we help you?" style="height:150px;"></textarea><br />
+                    <input class="btn btn-primary" type="submit" value="Send" ><br><br />
+                </form>
+            </div>
+            <div class="col-md-4">
+                <b>Customer service:</b> <br />
+                Phone: +91 63888 83615 (IND),<br /><span>+1 321-312-2671 (US),</span><br />
+                E-mail: <a href="#">infotechgiantofficial@gmail.com</a><br />
+
+                <br />
+                <b>Headquarter:</b><br />
+                138/089 (138/107),<br />
+                Fatehganj Gallamandi, Aminabad<br />
+                Uttar Pradesh 226018, INDIA<br />
+
+                <br />
+                <b>USA:</b><br />
+                New Jersey, <br />
+                Jersey City, New Jersey<br />
+                NJ 07306, United State<br />
+                
+            </div>
+        </div>
+    </div> -->
+
+    <div class="card mb-3">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        <img src="..." class="card-img-top" alt="...">
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Contact Us Section -->
+<div class="container">
+
+<h1 class="text-center">Contact us</h1><br />
+
+<div class="row">
+    <div class="col-md-8">
+        <form action="index.php" method="post">
+            <input class="form-control" name="name" id="name" placeholder="Full Name" /><br />
+            <input class="form-control" name="phone" id="phone" placeholder="Primary Phone" /><br />
+            <input class="form-control" name="email" id="email" placeholder="E-mail Address" /><br />
+            <textarea class="form-control" name="message" id="message" placeholder="Enter Your Message"
+                style="height:150px;"></textarea><br />
+            <input class="btn btn-primary" type="reset" value="Reset">
+            <input class="btn btn-primary" type="submit" value="Send" /><br /> <br />
+        </form>
+    </div>
+    <div class="col-md-4">
+        <b>Customer service:</b> <br />
+        Phone: +91 63888 83615 (IND),<br /><span>+1 321-312-2671 (US),</span><br />
+        E-mail: <a href="#">infotechgiantofficial@gmail.com</a><br />
+
+        <br />
+        <b>Headquarter:</b><br />
+        138/089 (138/107),<br />
+        Fatehganj Gallamandi, Aminabad<br />
+        Uttar Pradesh 226018, INDIA<br />
+
+        <br />
+        <b>USA:</b><br />
+        New Jersey, <br />
+        Jersey City, New Jersey<br />
+        NJ 07306, United State<br />
+    </div>
+</div>
+</div>
+
+
+
+
+
+<?php
+
+// making connection to the database creating connection 
+$servername = "localhost";
+$username = "root";
+$password ="";
+$database = "contactus";
+
+
+$conn = mysqli_connect($servername , $username , $password , $database);
+
+// if (isset($_POST['name'], $_POST['phone'],$_POST['email'],$_POST['message'])) {
+// echo "send messsage to us";
+// }
+
+
+
+
+
+if (!$conn){
+    echo("connection not successful");
+}
+else{
+   echo ("connection successful") ;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $name =  htmlspecialchars($_POST['name']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $email =  htmlspecialchars($_POST['email']);
+    $message =  htmlspecialchars($_POST['message']);
+    
+    }
+
+    // $name = htmlspecialchars($_REQUEST['fname']); 
+
+
+
+// SQL QUERY TO BE EXECUTED 
+
+
+
+$sql = "INSERT INTO `contactinfotech` (`name`, `phone`, `email`, `message`) VALUES ('$name','$phone','$email','$message')";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+echo("your name email and message is successfully sent to InfoTech Giant");
+}
+else{
+    echo("not inserted");
+}
+
+?>
+
+
+<!-- footer -->
+
+<div class="bg-gray-200">
+        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p class="text-gray-500 text-sm text-center sm:text-left">Copyright ©
+                2020 All Rights Reserved —<a
+                    href="" rel="noopener noreferrer" class="text-gray-600 ml-1"
+                    target="_blank">@InfoTech Giant</a></p><span
+                class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start"><a
+                    class="text-gray-500" href="#"><svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg></a><a class="ml-3 text-gray-500" href="#"><svg fill="currentColor" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                        <path
+                            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                        </path>
+                    </svg></a><a class="ml-3 text-gray-500" href="#"><svg fill="none" stroke="currentColor"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
+                        viewBox="0 0 24 24">
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg></a><a class="ml-3 text-gray-500" href="#"><svg fill="currentColor" stroke="currentColor"
+                        stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5"
+                        viewBox="0 0 24 24">
+                        <path stroke="none"
+                            d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
+                        </path>
+                        <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                    </svg></a></span>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
+</html>
---
+++ services.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="index.css">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script type="text/javascript" src="index.js"></script>
+     <link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet'>
+      <!-- sticky icons -->
+      <link rel="stylesheet" type="text/css" href="sticky.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>InfoTech Giant</title>
+</head>
+
+<body>
+    <!-- sticky icons -->
+    <div class="icons">
+        <ul>
+
+
+            <a href="https://www.facebook.com/InfoTechGiantOfficial">
+                <li class="facebook"><i class="fa fa-facebook"></i></li>
+            </a>
+            <a href="#">
+                <li class="twitter"><i class="fa fa-twitter"></i></li>
+            </a>
+            <a href="#">
+                <li class="linkedin"><i class="fa fa-linkedin"></i></li>
+            </a>
+            <a href="#">
+                <li class="instagram"><i class="fa fa-instagram"></i></li>
+            </a>
+
+        </ul>
+    </div>
+    
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-primary">
+        <a class="navbar-brand" href="#"><strong class="company">InfoTech Giant</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li> -->
+                <li class="nav-item active">
+                    <a class="nav-link" href="services.html">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Give us Feedback</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <!-- my code using bootstrap yash coding -->
+
+    <div class="card mb-3">
+        <img src="website2.jpg" class="card-img-top" alt="..." height="500px">
+        <div class="card-body">
+            <h2 class="card-title">WEBSITE</h2>
+            <p class="font-italic">We have best designs and simple responsive UI to make your customer use it easily.
+                Our team will also assist you if you want personalized designs according to you. Get you own website and
+                make your presence online. Contact us or mail us at infotechgiantofficial@gmail.com and get quote.</p>
+            <!-- button for contact us -->
+            <a href="#" class="btn btn-primary btn-block" role="button" aria-pressed="true">Contact-Us</a>
+
+        </div>
+    </div>
+
+
+    <div class="card mb-3">
+        <img src="seo.jpg" class="card-img-top" alt="..." height="500px">
+        <div class="card-body">
+            <h2 class="card-title">SEO READY WEBSITE</h2>
+            <p class="font-italic">We provide SEO ready and fully optimized websites so that your website get ranked in
+                top google results in order to boost your business online, increase your sales, website traffic and
+                increased customer engagement.</p>
+            <!-- button for contact us -->
+            <a href="#" class="btn btn-primary btn-block" role="button" aria-pressed="true">Contact-Us</a>
+
+        </div>
+    </div>
+
+
+
+
+
+
+</body>
+
+</html>
